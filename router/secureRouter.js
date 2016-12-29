32028646337917a497cce67c05f32e69ff4ddfc7
svsh227 <shubham.v@daffodilsw.com>
2016-12-29 13:35:15 +0530

    commit: set timings

--- router/secureRouter.js
+++ router/secureRouter.js
@@ -11,6 +11,7 @@ router.post('/user/totalbreaktime_idletime', controllerIndex.timings.getTotalBre
 
 router.post('/user/changeIsApproved', controllerIndex.timings.changeIsApproved);
 router.post('/user/breaktimelist', controllerIndex.timings.breaktimelist);
+router.post('/user/setDefaultTimings', controllerIndex.timings.setDefaultTimings);
 router.post('/user/detail', controllerIndex.user.getUserDetail);
 router.post('/user/changepassword', controllerIndex.user.changePassword);
 
